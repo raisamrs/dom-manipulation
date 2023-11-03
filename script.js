17adc1781b5017ad26d16be362863c78916674ae
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-11-03 17:15:30 -0300

    exercício 3 resolvido & modificação da estrutura do código para funções

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 // Exercício 1 - Alterando o texto
 // Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos.
-const emptyParagraph = document.querySelectorAll('.center-content p')[1];
-emptyParagraph.innerText = 'Daqui a dois anos, me vejo bem sucedida.';
-
+const changeText2ndParagraph = (element, text) => {
+  const emptyParagraph = document.querySelectorAll(element)[1];
+  emptyParagraph.innerText = text;
+};
+changeText2ndParagraph('.center-content p', 'Daqui a dois anos, me vejo bem sucedida.');
 // Exercício 2 - Alterando a cor - elemento amarelo
 // Crie e execute uma função que mude a cor de fundo do elemento amarelo para rgb(76, 164, 109).
-const leftRighElement = document.getElementsByClassName('main-content')[0];
-leftRighElement.style.backgroundColor = 'rgb(76, 164, 109)';
+const mainContentBgColor = (element) => {
+  const mainContent = document.getElementsByClassName(element)[0];
+  mainContent.style.backgroundColor = 'rgb(76, 164, 109)';
+};
+mainContentBgColor('main-content');
+
+// Exercício 3 - Alterando a cor - elemento vermelho
+// Crie e execute uma função que mude a cor de fundo do elemento vermelho para branco.
+
+const centerContentBgColor = (element) => {
+  const centerContent = document.getElementsByClassName(element)[0];
+  centerContent.style.backgroundColor = 'rgb(255, 255, 255)';
+};
+
+centerContentBgColor('center-content');
