d6a43b5b511a31d170ba061314644c92f81e89ad
Raisa Moreno <moreno.raisa.cursos@gmail.com>
2023-11-03 16:40:45 -0300

    exercício 1 resolvido

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// Seu código aqui
+// Exercício 1 - Alterando o texto
+// Crie e execute uma função que mude o texto na tag <p>-----</p>, para uma descrição de como você se vê daqui a 2 anos.
+const emptyParagraph = document.querySelectorAll('.center-content p')[1];
+emptyParagraph.innerText = 'Daqui a dois anos, me vejo bem sucedida.';
